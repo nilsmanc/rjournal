553094d0058e0eb9bf454720654df9e2fc5e47ca
nilsmanc <etereaiberia@gmail.com>
2022-08-08 17:39:41 +0300

    deploy

--- utils/api/index.ts
+++ utils/api/index.ts
@@ -16,7 +16,7 @@ export const Api = (ctx?: NextPageContext | GetServerSidePropsContext): ApiRetur
   const token = cookies.rtoken
 
   const instance = axios.create({
-    baseURL: process.env.NEXT_APP_API_URL,
+    baseURL: process.env.REACT_APP_API_URL,
     headers: {
       Authorization: 'Bearer ' + token,
     },
